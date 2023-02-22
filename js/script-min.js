89fb77ba7847ad6f8300653f7755e9ab688b9518
unknown <annahrishko@ukr.net>
2023-02-22 16:03:45 +0200

    new_parcel new edit

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".accordion-wrap .accordion-section").eq(0).find(".section-sub-menu").addClass("active"),$(".section-name").click(function(e){e.preventDefault(),$(".section-name").removeClass("active"),$(this).addClass("active"),$(".section-sub-menu").removeClass("active"),$(this).next(".section-sub-menu").addClass("active")})});
